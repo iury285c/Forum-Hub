1eac4f237475bb28e9340a548298c8f4116d088f
iury285c <ig285c@gmail.com>
2024-07-16 21:39:15 -0300

    Delete topico/TopicoRepository.java

--- topico/TopicoRepository.java
+++
@@ -1,8 +0,0 @@
-package hub.forum.api.topico;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TopicoRepository extends JpaRepository<Topico, Long> {
-}
